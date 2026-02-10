ecb94b1ac024f58ca088caf3e6888afaaf2fe813
Anushankari1234 <Anushankari.Sivaponnappan@rootquotient.com>
2026-02-10 15:45:13 +0530

    Modified redis with docker

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    synchronize: false,
+    synchronize: true,
     logging: false,
     entities: ['src/models/*.ts'],
     migrations: ['src/migrations/*.ts'],
--- src/repositories/redisRepo.ts
+++ src/repositories/redisRepo.ts
@@ -1,40 +1,29 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
 
 export const redisClient = createClient({
-  url: 'redis://127.0.0.1:6379',
+  url: process.env.REDIS_URL || "redis://redis:6379",
 });
 
-redisClient.on('connect', () => {
-  console.log('Redis connected');
+redisClient.on("connect", () => {
+  console.log("Redis connected");
 });
 
-redisClient.on('error', (err) => {
-  console.error('Redis error', err);
+redisClient.on("error", (err) => {
+  console.error("Redis error", err);
 });
-
 
 (async () => {
   await redisClient.connect();
 })();
 
-
-const ACCESS_TOKEN_TTL = 60 * 60; 
+const ACCESS_TOKEN_TTL = 60 * 60;
 
 export const storeAccessToken = async (userId: number, token: string) => {
-  await redisClient.set(
-    `auth:user:${userId}`,
-    token,
-    { EX: ACCESS_TOKEN_TTL }
-  );
+  await redisClient.set(`auth:user:${userId}`, token, { EX: ACCESS_TOKEN_TTL });
 };
 
-
 export const blacklistToken = async (token: string) => {
-  await redisClient.set(
-    `blacklist:${token}`,
-    'true',
-    { EX: ACCESS_TOKEN_TTL }
-  );
+  await redisClient.set(`blacklist:${token}`, "true", { EX: ACCESS_TOKEN_TTL });
 };
 
 export const isTokenBlacklisted = async (token: string) => {
